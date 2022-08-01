1a2731846fc4d785898990714934863e6b26668e
Sea Baya <seabaya@shinoMacBook-Air.local>
2022-08-02 03:46:39 +0900

    Nuxt.jsの初期化

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,7 +4,7 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _752da428 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _58ebc008 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -19,7 +19,7 @@ export const routerOptions = {
 
   routes: [{
     path: "/",
-    component: _752da428,
+    component: _58ebc008,
     name: "index"
   }],
 
