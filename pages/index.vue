99cff8448a608c01a676d5c807a92673273ebd34
BayaSea0907 <seabaya@shinoMacBook-Air.local>
2022-08-21 17:55:44 +0900

    薬物乱用頭痛の漫画掲載ページを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <MagazineCard
-      :thumbnail-url="require('~/assets/images/yakubutsu_ranyo/thumbnail.png')"
+      :thumbnail-url="require('~/assets/images/yakubutsu_ranyo_zutsu/thumbnail.png')"
       title="薬物乱用頭痛になる社畜(全21話)"
       description="Twitterでバズったエッセイ漫画！
       仕事で疲れ果て、薬漬けの生活を繰り返した末路とは！？"
-      link-path="/yakubutsu_ranyo"
+      link-path="/yakubutsu_ranyo_zutsu"
     />
   </div>
 </template>
---
+++ pages/yakubutsu_ranyo_zutsu/2.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <NuxtLink to="/">🏠トップ</NuxtLink>＞
+    <NuxtLink to="/yakubutsu_ranyo_zutsu">薬物乱用頭痛</NuxtLink>＞
+    <NuxtLink to="/yakubutsu_ranyo_zutsu/1">10~14話</NuxtLink>
+
+    <h2>1~9話</h2>
+    <ul class="magazine-images">
+      <li v-for="magazineImage in magazineImages">
+        <h3>{{ magazineImage.title }}</h3>
+        <img :src="magazineImage.src" class="magazine-images__image">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        magazineImages: [
+          { title: '10話', src: require('~/assets/images/yakubutsu_ranyo_zutsu/10.jpg') },
+          { title: '11話', src: require('~/assets/images/yakubutsu_ranyo_zutsu/11.jpg') },
+          { title: '12話', src: require('~/assets/images/yakubutsu_ranyo_zutsu/12.jpg') },
+          { title: '13話', src: require('~/assets/images/yakubutsu_ranyo_zutsu/13.jpg') },
+          { title: '14話', src: require('~/assets/images/yakubutsu_ranyo_zutsu/14.jpg') },
+        ]
+      };
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .magazine-images {
+    &__image {
+      width: 600px;
+      height: auto;
+    }
+  }
+</style>
