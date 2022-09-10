fa01b26bd0e6fdf1976338cccb2c00584a475d75
BayaSea0907 <seabaya@shinoMacBook-Air.local>
2022-09-10 18:19:26 +0900

    トップページの漫画カードのサムネイルを解像度の高い画像に差し替え。あと、影の表現を追加

--- components/molecules/MagazineCard.vue
+++ components/molecules/MagazineCard.vue
@@ -30,15 +30,14 @@
 <style lang="scss" scoped>
 .card {
   display: flex;
-  padding: 5px;
   margin-bottom: 14px;
-  border-bottom: 1px solid $gray-1;
+  box-shadow: 5px 5px 5px $gray-1;
 }
 
 .card-thumbnail {
   display: inline-block;
   margin-right: 10px;
-  border: solid $gray-1;
+  border: solid $gray-3;
   width: 600px;
   height: auto;
 
