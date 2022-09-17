737816930265f33a314c2cfb0638395a406f4e19
BayaSea0907 <seabaya@shinoMacBook-Air.local>
2022-09-17 17:49:59 +0900

    Twitterタイムラインが長すぎてSPでスライドしづらいので、長さを短くする

--- components/atoms/TwitterTimeline.vue
+++ components/atoms/TwitterTimeline.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="twitter">
     <h3 class="twitter__title">Twitter</h3>
-    <a class="twitter-timeline" data-lang="ja" data-width="100%" data-height="550" data-theme="light" href="https://twitter.com/hokuro_90?ref_src=twsrc%5Etfw">Tweets by hokuro_90</a>
+    <a class="twitter-timeline" data-lang="ja" data-width="100%" data-height="400" data-theme="light" href="https://twitter.com/hokuro_90?ref_src=twsrc%5Etfw">Tweets by hokuro_90</a>
     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
   </section>
 </template>
