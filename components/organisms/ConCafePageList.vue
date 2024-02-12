052f255933fd7afcc08b69c2e06139afc1e4cd77
BayaSea0907 <seabaya@shinoMacBook-Air.local>
2024-02-12 15:29:37 +0900

    「女になったので、コンカフェ始めました」のページ追加と、薬物乱用頭痛のリンク復活

---
+++ components/organisms/ConCafePageList.vue
@@ -0,0 +1,25 @@
+<template>
+  <BasePageList
+    :page-items="magazines"
+  />
+</template>
+
+<script>
+  import BasePageList from './../molecules/BasePageList.vue'
+
+  export default {
+    components: {
+      BasePageList,
+    },
+    data() {
+      return {
+        magazines: [
+          { title: '01〜03話', link_path: '/saikopasu_joshi/1' },
+          { title: '04〜07話', link_path: '/saikopasu_joshi/2' },
+          { title: '08〜11話', link_path: '/saikopasu_joshi/3' },
+          { title: '12〜14話', link_path: '/saikopasu_joshi/4' },
+        ]
+      };
+    }
+  }
+</script>
