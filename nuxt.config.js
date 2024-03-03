f4b9d5358e8734f01e44686a67139aafe7892b9f
BayaSea0907 <seabaya@shinoMacBook-Air.local>
2024-03-03 15:53:37 +0900

    ツイッターカード用の画像とタグを追加する

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,14 +25,27 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes, shrink-to-fit=no' },
-      { name: 'author', content: 'ばや' },
+      { name: 'author', content: 'ほくろ' },
       { name: 'description', content: '日常のひとこまを漫画に描いて発信している「ほくろ」さんを応援するサイトです。' },
+      { name: 'keywords', content: '漫画, マンガ, エッセイ, エッセイマンガ, エッセイ漫画, ４コマ' },
+
+      // Open Graph Protocol
+      { property: 'og:local',  content: 'ja_JP' },
+      { property: 'og:type',  content: 'website' },
+      { property: 'og:url',   content: 'https://hokuro-books.web.app' },
+      { property: 'og:image', content: 'https://hokuro-books.web.app/og_image.png' },
+      { property: 'og:site_name', content: 'ほくろマンガ' },
+      { property: 'og:title', content: 'ほくろマンガ' },
+      { property: 'og:description', content: '日常のひとこまを漫画に描いて発信している「ほくろ」さんを応援するサイトです。' },
+
+      // Twitter
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@hokuro_90' },
+
       // Google Search Console
       { name: 'google-site-verification', content: 'h2hijYFBqvtUSBQJ1Q3Xf2TdR8c7gCbs7AVKTMKREWQ' },
-      //
       // クローリングの無効化
       // { name: 'robots', content: 'noindex' },
-      //
     ],
     link: [
       // ファビコン
